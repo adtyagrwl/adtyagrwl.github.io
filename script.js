f3d39363c5a17952352553dcf8c962bd8599da8d
adtyagrwl <78271189+adtyagrwl@users.noreply.github.com>
2021-12-08 07:57:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 <script>
 document.addEventListener('keydown', function(event){
 	if(event.key === "Escape"){
-		<a>
+		<a href="Aaditay Agarwal CV Dec 2021.pdf" target="_blank"><span class="button">&copy; Aaditay Agarwal.</span></a>
 	}
 });
 </script>
